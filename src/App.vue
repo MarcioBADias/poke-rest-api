bf918d3834d88472c880eaae1a63f1a1b9b1160f
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-14 22:03:28 -0300

    Criação de projeto setando Background

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+  #app{
+    background-image: url(../public/img/poke-bg.jpg);
+    background-size: cover;
+  }
+</style>
