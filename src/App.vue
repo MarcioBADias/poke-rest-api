f567e523a5eaab742436c9629188e0d28ab32d77
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-16 16:43:32 -0300

    Divisão do projeto em componentes para demanda
    Resolvi criar um componente base "Pokemon" para englobar todos os outros e ser chamado no App, e já dividi a página em tres componentes, um de listas, um para funções de pesquisa e um com os detalhs do item selecionado afim de atender o desafio.
    
    Reset já ciado no APP base

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <v-app>
-    
+    <Pokemon />
   </v-app>
 </template>
 
 <script>
+import Pokemon from './components/Pokemon.vue'
 
 export default {
-  name: 'App',
-
-  components: {},
-
-  data: () => ({
-    //
-  }),
+  comments: {
+    Pokemon
+  }
 };
 </script>
 
 <style>
-  #app{
-    background-image: url(../public/img/poke-bg.jpg);
-    background-size: cover;
+  @import url('https://fonts.googleapis.com/css2?family=Changa:wght@400;700&family=MedievalSharp&family=Paytone+One&family=Staatliches&display=swap');
+
+  *{
+    box-sizing: border-box;
+    font-family: 'Changa', sans-serif;
+    margin:0;
+    padding: 0;
   }
 </style>
