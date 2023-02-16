e9bc55d500522f077066730085a12fd62470330c
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-16 18:15:43 -0300

    Ajust de BG e criação de container no Pokemon Page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <v-app>
-    <Pokemon />
+  <v-app id="app">
+      <PokemonPage />
   </v-app>
 </template>
 
 <script>
-import Pokemon from './components/Pokemon.vue'
+import PokemonPage from './components/PokemonPage.vue';
 
 export default {
-  comments: {
-    Pokemon
+  components: {
+    PokemonPage
   }
 };
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Changa:wght@400;700&family=MedievalSharp&family=Paytone+One&family=Staatliches&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Changa:wght@400;700&family=MedievalSharp&family=Paytone+One&family=Staatliches&display=swap');
 
-  *{
-    box-sizing: border-box;
-    font-family: 'Changa', sans-serif;
-    margin:0;
-    padding: 0;
-  }
+*{
+  box-sizing: border-box;
+  font-family: 'Changa', sans-serif;
+  margin:0;
+  padding: 0;
+}
+
 </style>
---
+++ src/components/PokemonPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <h1>Pokemon's</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PokemonPage',
+    };
+</script>
+
+<style>
+    .container{
+        background: url(../../public/assets/img/poke-bg.jpg) no-repeat center / cover;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        padding: 1rem;
+        width: calc(100%-20px);
+        min-height: calc(100%-20px);
+        font-family: 'Paytone One', sans-serif;
+        font-size: 1rem;
+    }
+    
+    h1{
+        background: #fefefe;
+        border: #1E90FF solid 3px;
+        box-shadow: 0 15px 30px #fefefe,
+                    0 10px 10px #fefefe;
+        border-radius: 10px;
+        padding: 0 .5rem;
+    }
+</style>
