f42f3644fc89113f18aba316020210bd88023e37
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-17 11:03:01 -0300

    Incluindo infos no Detail

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -8,10 +8,66 @@
                 <h2>{{ pokemon.name }}</h2>
                 <div class="property">
                     <div class="left">
-                        Base Experience
+                        HP
                     </div>
                     <div class="right">
-                        {{ pokemon.base_experience }} XP
+                        {{ pokemon.stats[0].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Attack
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[1].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Defese
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[2].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Special Attack
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[3].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Special Defense
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[4].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Speed
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[5].base_stat }}
+                    </div>
+                </div>
+                <h3>Pokemon Types</h3>
+                <div class="types">
+                    <div class="type" 
+                        v-for="(value, index) in pokemon.types"
+                        :key="'value'+index">
+                        {{ value.type.name }}
+                    </div>
+                </div>
+                <h3>Abilities</h3>
+                <div class="abilities">
+                    <div class="ability" 
+                        v-for="(value, index) in pokemon.abilities"
+                        :key="'value'+index">
+                        {{ value.ability.name }}
                     </div>
                 </div>
             </div>
@@ -42,6 +98,7 @@
                             return resp.json();
                     })
                     .then(data => {
+                        console.log(data);
                         this.pokemon = data;
                         this.show = true;
                     })
@@ -128,4 +185,38 @@
     .right{ 
             float: right;
     }
+
+    h3{
+        border-bottom: 1px solid #ccc;
+        max-width: 400px;
+        width: 90%;
+    }
+
+    .types, .abilities{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        max-width: 400px;
+        width: 90%;
+    }
+
+    .type, .ability{
+        border-radius: 20px;
+        color: #fff;
+        letter-spacing: 2px;
+        margin: 1rem 1rem 1rem 0;
+        padding: .5rem 1rem;
+        text-transform: capitalize;
+        word-wrap: none;
+        word-break: keep-all;
+    }
+
+    .type{
+        background-color: #0a2e50;
+    }
+
+    .ability{
+        background-color: #c73015;
+    }
+
 </style>
