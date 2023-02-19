6fee30ad94b752c1b7e04eb8088141c2f5bf5e78
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-18 22:12:47 -0300

    Ajuste de icone que estava quebrando

---
+++ components/PokemonDetail.vue
@@ -0,0 +1,249 @@
+<template>
+    <div class="detail" @click="clickOutside">
+        <div class="detail-view" v-if="show">
+            <div v-if="pokemon" class="image">
+                <img :src="imageUrl+pokemon.id+'.png'" alt="">
+            </div>
+            <div v-if="pokemon" class="data">
+                <h2>{{ pokemon.name }}</h2>
+                <div class="property">
+                    <div class="left">
+                        HP
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[0].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Attack
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[1].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Defese
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[2].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Special Attack
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[3].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Special Defense
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[4].base_stat }}
+                    </div>
+                </div>
+                <div class="property">
+                    <div class="left">
+                        Speed
+                    </div>
+                    <div class="right">
+                        {{ pokemon.stats[5].base_stat }}
+                    </div>
+                </div>
+                <h3 class="subTitle">Pokemon Types</h3>
+                <div class="types">
+                    <div class="type" 
+                        v-for="(value, index) in pokemon.types"
+                        :key="'value'+index">
+                        {{ value.type.name }}
+                    </div>
+                </div>
+                <h3 class="subTitle">Abilities</h3>
+                <div class="abilities">
+                    <div class="ability" 
+                        v-for="(value, index) in pokemon.abilities"
+                        :key="'value'+index">
+                        {{ value.ability.name }}
+                    </div>
+                </div>
+            </div>
+            <h2 v-else>
+                O pokemon não foi encontrado
+            </h2>
+            <button class="close" @click="closeDetail">
+                Close
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: [
+            'pokemonUrl',
+            'imageUrl'
+        ],
+
+        data: () => {
+            return {
+                show: false,
+                pokemon: {}
+            }
+        },
+
+        methods: {
+            getPokeAPI() {
+                let req = new Request(this.pokemonUrl);
+                fetch(req)
+                    .then(resp =>{
+                        if(resp.status === 200)
+                            return resp.json();
+                    })
+                    .then(data => {
+                        this.pokemon = data;
+                        this.show = true;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            },
+
+            clickOutside(event) {
+                if (!event.target.closest('.detail-view')) {
+                    this.closeDetail();
+                }
+            },
+            
+            closeDetail() {
+                this.$emit('closeDetail');
+            }
+        },
+
+        created() {
+            this.getPokeAPI();
+        }
+    }
+</script>
+
+<style>
+    .detail {
+        align-items: flex-start;
+        background:rgba(0, 0, 0, 0.700);
+        display: flex;
+        height: 100%;
+        justify-content: center;
+        left: 0;
+        padding: 9rem 1rem 1rem;
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+
+    .detail-view{
+        align-items: center;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 15px 30px rgba(0,0,0,.2),
+                    0 10px 10px rgba(0,0,0,.2);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        max-width: 510px;
+        padding: 5rem 0 0;
+        position: relative;
+        width: 100%;
+    }
+
+    .image{
+        align-items: center;
+        background-color: #efefef;
+        border-radius: 50%;
+        box-shadow: 0 15px 30px rgba(0,0,0,.2),
+                    0 10px 10px rgba(0,0,0,.2);
+        display: flex;
+        height: 120px;
+        justify-content: center;
+        overflow: hidden;
+        position: absolute;
+        top: -60px;
+        width: 120px;
+    }
+
+    h2{
+        text-transform: capitalize;
+    }
+
+    .data{
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-bottom: 2rem;
+        width: 100%;
+    }
+
+    .property{
+        width: 90%;
+        max-width: 400px;
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 1rem;
+    }
+
+    .left{ 
+            float: left;
+    }
+
+    .right{ 
+            float: right;
+    }
+
+    .subTitle{
+        border-bottom: 1px solid #ccc;
+        max-width: 400px;
+        width: 90%;
+    }
+
+    .types, .abilities{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        max-width: 400px;
+        width: 90%;
+    }
+
+    .type, .ability{
+        border-radius: 20px;
+        color: #fff;
+        letter-spacing: 2px;
+        margin: 1rem 1rem 1rem 0;
+        padding: .5rem 1rem;
+        text-transform: capitalize;
+        word-wrap: none;
+        word-break: keep-all;
+    }
+
+    .type{
+        background-color: #1E90FF;
+    }
+
+    .ability{
+        background-color: #c73015;
+    }
+
+    .close{
+        background: #0a2e50;
+        outline: none;
+        border: none;
+        border-radius: 20px;
+        color: #efefef;
+        padding: 1rem 1rem;
+        margin-bottom: 2rem;
+        font-size: 1.2rem;
+        cursor: pointer
+    }
+
+</style>
---
+++ components/PokemonPage.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="container">
+        <h1>Pokemon's</h1>
+        <PokemonSearch 
+            :apiUrl="apiUrl"
+            @setPokemonUrl="setPokemonUrl"
+        />
+        <PokemonList 
+            :imageUrl="imageUrl" 
+            :apiUrl="apiUrl"
+            @setPokemonUrl="setPokemonUrl"
+        />
+        <PokemonDetail 
+            v-if="showDetail"
+            :pokemonUrl="pokemonUrl"
+            :imageUrl="imageUrl"
+            @closeDetail="closeDetail"
+        />
+    </div>
+</template>
+
+<script>
+    import PokemonSearch from './PokemonSearch.vue';
+    import PokemonList from './PokemonList.vue';
+    import PokemonDetail from './PokemonDetail.vue';
+    
+    export default {
+        name: 'PokemonPage',
+
+        data: () => {
+           return {
+            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+            apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
+            pokemonUrl:'',
+            showDetail: false
+           } 
+        },
+        components: {
+            PokemonSearch,
+            PokemonList,
+            PokemonDetail
+        },
+        methods: {
+            setPokemonUrl(url) {
+                this.pokemonUrl = url;
+                this.showDetail = true;
+            },
+            closeDetail() {
+                this.pokemonUrl = '';
+                this.showDetail = false;
+            }
+        }
+    };
+</script>
+
+<style>
+    .container{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        padding: 1rem;
+        width: calc(100%-20px);
+        min-height: calc(100%-20px);
+        font-family: 'Paytone One', sans-serif;
+        font-size: 1rem;
+    }
+    
+    h1{
+        background: #fefefe;
+        box-shadow: 0 15px 30px rgba(0,0,0,.2),
+                    0 10px 10px rgba(0,0,0,.2);
+        border-radius: 10px;
+        color: #0a2e50;
+        margin-bottom: 1rem;
+        padding: 0 1rem;
+    }
+</style>
